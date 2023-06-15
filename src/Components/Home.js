f4df10457bf004203f2b732843507813da78fdf3
jfmemon <jannatul15-2497@diu.edu.bd>
2023-06-15 19:11:33 +0600

    just user delete confirmation(client site)

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,13 +3,19 @@ import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
     const users = useLoaderData();
+    const handleDeleteUser = user => {
+        const agree = window.confirm(`Are you sure, you want to delete ${user.name}?`)
+        if(agree) {
+            console.log(`Deleted user id is ${user._id}`)
+        }
+    }
     return (
         <div>
             <h3>Users: {users.length}</h3>
             <div>
                 {
                     users.map(user => <p key={user._id}>
-                        {user.name} {user.email}
+                        {user.name} {user.email} <button onClick={() => handleDeleteUser(user)}>X</button>
                     </p>)
                 }
             </div>
