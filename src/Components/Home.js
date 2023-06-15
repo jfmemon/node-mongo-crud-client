3eb678cc9cbae0b96e2840801a6fd06ee8c0b9f0
jfmemon <jannatul15-2497@diu.edu.bd>
2023-06-15 20:01:50 +0600

    delete specific user(client site)

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
     const users = useLoaderData();
+    const [displayUsers, setDisplayUsers] = useState(users);
+
     const handleDeleteUser = user => {
         const agree = window.confirm(`Are you sure, you want to delete ${user.name}?`)
         if(agree) {
-            console.log(`Deleted user id is ${user._id}`)
+            fetch(`http://localhost:5000/users/${user._id}`, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.deletedCount > 0) {
+                    const remainingUsers = displayUsers.filter(usr => usr._id !== user._id);
+                    setDisplayUsers(remainingUsers);
+                }
+            })
         }
     }
     return (
         <div>
-            <h3>Users: {users.length}</h3>
+            <h3>Users: {displayUsers.length}</h3>
             <div>
                 {
-                    users.map(user => <p key={user._id}>
+                    displayUsers.map(user => <p key={user._id}>
                         {user.name} {user.email} <button onClick={() => handleDeleteUser(user)}>X</button>
                     </p>)
                 }
